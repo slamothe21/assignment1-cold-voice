b070cf3ec298fd7b0d7dd271a21966ce91245433
sman360 <sl2938@princeton.edu>
2024-10-03 22:26:58 -0400

    commit 6

--- server.go
+++ server.go
@@ -7,7 +7,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"os"
 )
 
@@ -18,7 +20,22 @@ const RECV_BUFFER_SIZE = 2048
  * Print received message to stdout
  */
 func server(server_port string) {
+	address := fmt.Sprintf("127.0.0.1:%s", server_port)
 
+	// Start to listen for incoming connections
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		// handle error
+		log.Fatalf("Error starting the server on port %s: %v", server_port, err)
+
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go handleConnection(conn)
+	}
 }
 
 // Main parses command-line arguments and calls server function
