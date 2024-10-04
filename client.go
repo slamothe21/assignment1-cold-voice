6a21852d6597f71ba1c59691d8251ec13dc58ef4
sman360 <sl2938@princeton.edu>
2024-10-03 20:36:33 -0400

    commit 1

--- client.go
+++ client.go
@@ -7,17 +7,47 @@
 package main
 
 import (
+	"io"
 	"log"
+	"net"
 	"os"
 )
 
 const SEND_BUFFER_SIZE = 2048
 
-/* TODO: client()
- * Open socket and send message from stdin.
- */
+// client opens a socket and sends message from stdin
 func client(server_ip string, server_port string) {
+	// Connect to the server
+	conn, err := net.Dial("tcp", server_ip+":"+server_port)
+	if err != nil {
+		log.Fatalf("Failed to connect to server: %v", err)
+	}
+	defer conn.Close()
 
+	// Create a buffer to read from stdin
+	buf := make([]byte, SEND_BUFFER_SIZE)
+
+	for {
+		// Read data from stdin
+		n, err := os.Stdin.Read(buf)
+		if err != nil {
+			// Handle EOF gracefully
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("Failed to read from stdin: %v", err)
+		}
+
+		// Send the data to the server in chunks
+		totalSent := 0
+		for totalSent < n {
+			sent, err := conn.Write(buf[totalSent:n])
+			if err != nil {
+				log.Fatalf("Failed to send data: %v", err)
+			}
+			totalSent += sent
+		}
+	}
 }
 
 // Main parses command-line arguments and calls client function
