ec984309362e9c48c9d1d83ed083a3e5bac420e1
sman360 <sl2938@princeton.edu>
2024-10-05 00:09:30 -0400

    here we go

--- server.go
+++ server.go
@@ -7,10 +7,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 )
 
 const RECV_BUFFER_SIZE = 2048
@@ -25,10 +27,8 @@ func server(server_port string) {
 	// Start to listen for incoming connections
 	ln, err := net.Listen("tcp", address)
 	if err != nil {
-		// handle error
 		log.Fatalf("Error starting the server on port %s: %v", server_port, err)
 	}
-
 	fmt.Printf("Server is listening on %s\n", address)
 
 	for {
@@ -37,29 +37,38 @@ func server(server_port string) {
 			log.Printf("Error accepting client connection: %v", err)
 			continue // Move on to the next connection attempt
 		}
-		// Handle the connected client in a dedicated function
-		handleConnection(conn)
+		// Handle the connected client in a goroutine for concurrency
+		go handleConnection(conn)
 	}
 }
-func handleConnection(conn net.Conn) {
-	// Make sure the connection is closed once we're done
-	defer conn.Close()
-	// Create a buffer to store the received data
-	buf := make([]byte, RECV_BUFFER_SIZE)
 
-	// Keep reading data from the client until they disconnect or there's an error
+/* TODO: handleConnection()
+ * Handle an individual client connection
+ */
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
 	for {
-		n, err := conn.Read(buf)
+		// Create a buffer and read the message
+		message, err := reader.ReadString('\n')
 		if err != nil {
-			// Handle EOF gracefully
 			if err == io.EOF {
 				fmt.Printf("Client at %s disconnected\n", conn.RemoteAddr().String())
 				break
 			}
-			log.Printf("error reading from client: %v", err)
-
+			log.Printf("Error reading from client: %v", err)
+			break
 		}
-		buf = buf[:n]
-		fmt.Print(string(buf[:n]))
+		fmt.Print("Message received:", message)
 	}
+}
+
+// Main parses command-line arguments and calls server function
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: ./server [server port]")
+	}
+	server_port := os.Args[1]
+	server(server_port)
 }
